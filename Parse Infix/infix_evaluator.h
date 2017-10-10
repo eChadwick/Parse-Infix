6c798038a6bd1f27331f38ec62b087d9a7b0547f
Eric Chadwick <eChadwick@users.noreply.github.com>
2017-10-10 17:26:23 -0500

    Move things from .cpp to .h file

--- Parse Infix/infix_evaluator.h
+++ Parse Infix/infix_evaluator.h
@@ -13,6 +13,16 @@ public:
 //private:
 	stack<int>operands;
 	stack<string>operators;
+	const std::map<std::string, int> precedence = {
+        	{ "!", 8 },{ "!!", 8 },{ "++", 8 },{ "--", 8 },
+        	{ "^", 7 },
+        	{ "*", 6 },{ "/", 6 },{ "%", 6 },
+        	{ "+", 5 },{ "-", 5 },
+        	{ ">", 4 },{ ">=", 4 },{ "<", 4 },
+        	{ "==", 3 },{ "!=", 3 },
+        	{ "&&", 2 },
+        	{ "||", 1 },
+    	};
 
 	// evaluates the stacks as far as possible and pushes the result to operand stack.
 	// @param precedence is the precedence of the next operator to be added to the operator stack.
@@ -26,4 +36,6 @@ public:
 	// @param operator_string is the string of one or many repeated + and/or -  and/or !
 	// @return is the value of operand after unary operations have been applied.
 	int eval_unaries(int start, int length, int operand, string& infix_string);
+	
+	int evaluate(string input);
 };
