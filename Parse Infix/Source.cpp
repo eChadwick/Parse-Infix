cfc40333ae532b37a2a2814d6f8d217d55aec3a0
Alec Sears <alec.xvs@gmail.com>
2017-10-11 16:19:15 -0500

    Basic impl of evaluate

--- Parse Infix/Source.cpp
+++ Parse Infix/Source.cpp
@@ -5,31 +5,8 @@ using namespace std;
 
 void main() {
 	infix_evaluator test;
-	string test_string = "10+!!----5";
+	string test_string = "10+----5";
+	test.evaluate(test_string);
 	cout << endl << "Evaluating unary: " << test.eval_unaries(3, 6, 5, test_string);
-	test_string = "10+!!!----5";
-	cout << endl << "Evaluating unary: " << test.eval_unaries(3, 7, 5, test_string);
-	test_string = "10+!!++----5";
-	cout << endl << "Evaluating unary: " << test.eval_unaries(3, 8, 5, test_string);
-	test_string = "10+!!+--5";
-	cout << endl << "Evaluating unary: " << test.eval_unaries(3, 5, 5, test_string);
-	test_string = "10+!!+++5";
-	cout << endl << "Evaluating unary: " << test.eval_unaries(3, 5, 5, test_string);
-	test_string = "10+++5";
-	cout << endl << "Evaluating unary: " << test.eval_unaries(3, 2, 5, test_string);
-	test_string = "10+!0";
-	cout << endl << "Evaluating unary: " << test.eval_unaries(3, 1, 0, test_string);
-	test_string = "10+!1";
-	cout << endl << "Evaluating unary: " << test.eval_unaries(3, 1, 1, test_string);
-	test_string = "10+!!0";
-	cout << endl << "Evaluating unary: " << test.eval_unaries(3, 2, 0, test_string);
-
-	test_string = "10+++$5";
-	cout << endl << "Evaluating unary: " << test.eval_unaries(3, 4, 5, test_string);
-	test_string = "10+++500";
-	cout << endl << "Evaluating unary: " << test.eval_unaries(3, 5, 5, test_string);
-
-	test_string = "10+++5++5";
-	cout << endl << "Evaluating unary: " << test.eval_unaries(3, 5, 5, test_string);
 	getchar();
 }
