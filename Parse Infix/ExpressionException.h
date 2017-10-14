b2b5093ad775842125db7f95c383f47fb3fa3f7d
Alec Sears <alec.xvs@gmail.com>
2017-10-14 15:43:11 -0500

    Restore comments

--- Parse Infix/ExpressionException.h
+++ Parse Infix/ExpressionException.h
@@ -3,9 +3,13 @@
 #include <sstream>
 #include <stdexcept>
 
+// A wrapper class around standard exception.  Adds the index of the error to the error message.
 class ExpressionException : public std::runtime_error
 {
 public:
+	// Constructor for expression_exception
+	// @param index is the index in the string where the error was detected.
+	// @param message is the error message thrown.
 	ExpressionException(unsigned int index, std::string message);
 };
 
