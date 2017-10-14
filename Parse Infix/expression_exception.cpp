a14ffc87b8eb6ff76da8ac00444519bc12cac4df
Alec Sears <alec.xvs@gmail.com>
2017-10-14 12:57:36 -0500

    Exception fixes

--- Parse Infix/expression_exception.cpp
+++ Parse Infix/expression_exception.cpp
@@ -1,11 +1,4 @@
 #include "expression_exception.h"
 
-expression_exception::expression_exception(unsigned int index, std::string message) : index(index), message(message) {}
-
-const char * expression_exception::what()
-{
-	std::ostringstream msg_stream("Error in expression at ");
-	msg_stream << index << ": " << message;
-	return msg_stream.str().c_str();
-}
-
+expression_exception::expression_exception(unsigned int index, std::string message)
+	: runtime_error("Error in expression at " + std::to_string(index) + ": " + message) {}
--- Parse Infix/expression_exception.h
+++ Parse Infix/expression_exception.h
@@ -4,13 +4,9 @@
 #include <stdexcept>
 
 class expression_exception :
-	public std::exception
+	public std::runtime_error
 {
 public:
 	expression_exception(unsigned int index, std::string message);
-	const char* what();
-private:
-	const std::string message;
-	const unsigned int index;
 };
 
